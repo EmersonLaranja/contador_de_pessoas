38f48000e63e154f1b30c8f4166f00f89f0c7064
andreocunha <andreoliveiracunha20@gmail.com>
2021-01-31 23:08:25 -0300

    .

--- public/index.js
+++ public/index.js
@@ -1,16 +1,19 @@
-let socket = new WebSocket("ws://contador-de-pessoas.herokuapp.com");
+const socket = new WebSocket("ws://contador-de-pessoas.herokuapp.com");
 // let socket = new WebSocket("ws://192.168.15.10:4000");
-let temp = document.getElementById("temp")
-let umid = document.getElementById("umidade")
-let data = document.getElementById("data")
-let qtdPessoas = document.getElementById("contador")
+
+document.addEventListener("DOMContentLoaded", () => {
+    let temp = document.getElementById("temp")
+    let umid = document.getElementById("umidade")
+    let data = document.getElementById("data")
+    let qtdPessoas = document.getElementById("contador")
 
 
-socket.onmessage = event => {
-    const dados = event.data.split(",")
-    
-    temp.innerHTML = dados[0] + " °C"
-    umid.innerHTML = dados[1]
-    data.innerHTML = dados[2]
-    qtdPessoas.innerHTML = dados[3]
-}
+    socket.onmessage = event => {
+        const dados = event.data.split(",")
+        
+        temp.innerHTML = dados[0] + " °C"
+        umid.innerHTML = dados[1]
+        data.innerHTML = dados[2]
+        qtdPessoas.innerHTML = dados[3]
+    }
+})
